a071006991ec15728a8c099cf9cbfc90696892bf
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-23 13:33:09 -0300

    Implementando classe de dominio da ordem de serviço

---
+++ src/main/java/br/com/marcos/domain/OS.java
@@ -0,0 +1,120 @@
+package br.com.marcos.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import br.com.marcos.domain.enuns.Prioridade;
+import br.com.marcos.domain.enuns.Status;
+
+public class OS {
+
+	private Integer id;
+	private LocalDate dataAbertura;
+	private LocalDate dataFechamento;
+	private Integer prioridade;
+	private String observacao;
+	private Integer status;
+	private Tecnico tecnico;
+	private Cliente cliente;
+
+	public OS() {
+		super();
+		this.setDataAbertura(LocalDate.now());
+		this.setPrioridade(Prioridade.BAIXA);
+		this.setStatus(Status.ABERTO);
+	}
+
+	public OS(Integer id, Prioridade prioridade, String observacao, 
+			Status status, Tecnico tecnico, Cliente cliente) {
+		super();
+		this.id = id;
+		this.setDataAbertura(LocalDate.now());
+		this.prioridade = (prioridade == null) ? 0 : prioridade.getCod();
+		this.observacao = observacao;
+		this.status = (status == null) ? 0 : status.getCod();
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDate dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDate getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDate dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnun(this.prioridade);
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade.getCod();
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public Status getStatus() {
+		return Status.toEnun(this.status);
+	}
+
+	public void setStatus(Status status) {
+		this.status = status.getCod();
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OS other = (OS) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
