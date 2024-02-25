8c4f861171625ed7113ba31c8c66a11cb57138bc
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-25 12:46:45 -0300

    Criando perfil de dev

--- src/main/java/br/com/marcos/config/DevConfig.java
+++ src/main/java/br/com/marcos/config/DevConfig.java
@@ -1,6 +1,7 @@
 package br.com.marcos.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,15 +9,22 @@ import org.springframework.context.annotation.Profile;
 import br.com.marcos.servcices.DBService;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 
 
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String ddl;
+	
 	@Bean
-	public void instanciaDB() {
-		this.dbService.instaciaDB();
+	public boolean instanciaDB() {
+		if(ddl.equals("create")) {
+			this.dbService.instaciaDB();
+		}
+		
+		return false;
 	}
 }
