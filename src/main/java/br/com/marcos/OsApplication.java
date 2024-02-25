020638e1dbcc7768b4185301d68ac7c78ac05fa6
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-24 23:36:34 -0300

    Correção

--- src/main/java/br/com/marcos/OsApplication.java
+++ src/main/java/br/com/marcos/OsApplication.java
@@ -36,7 +36,7 @@ public class OsApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		Tecnico t1 = new Tecnico(null, "Marcos Gabriel", "826.074.260-81", "(74) 96666-8958");
-		Cliente c1 = new Cliente(null, "João", "826.074.260-88", "(78) 96666-5252");
+		Cliente c1 = new Cliente(null, "João", "604.943.160-47", "(78) 96666-5252");
 		OS os1 = new OS(null, Prioridade.ALTA, "Teste create os", Status.ADAMENTO, t1, c1);
 
 		t1.getList().add(os1);
