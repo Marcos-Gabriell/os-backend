b74406ce5a9061d738d50331e427c813bec7b8ac
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-25 12:32:57 -0300

    Criando perfil de test

--- src/main/java/br/com/marcos/OsApplication.java
+++ src/main/java/br/com/marcos/OsApplication.java
@@ -1,49 +1,13 @@
 package br.com.marcos;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.marcos.domain.Cliente;
-import br.com.marcos.domain.OS;
-import br.com.marcos.domain.Tecnico;
-import br.com.marcos.domain.enuns.Prioridade;
-import br.com.marcos.domain.enuns.Status;
-import br.com.marcos.repository.ClienteRepository;
-import br.com.marcos.repository.OSRepository;
-import br.com.marcos.repository.TecnicoRepository;
-
 @SpringBootApplication
-public class OsApplication implements CommandLineRunner {
-
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private OSRepository osRepository;
+public class OsApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Tecnico t1 = new Tecnico(null, "Marcos Gabriel", "826.074.260-81", "(74) 96666-8958");
-		Cliente c1 = new Cliente(null, "João", "604.943.160-47", "(78) 96666-5252");
-		OS os1 = new OS(null, Prioridade.ALTA, "Teste create os", Status.ADAMENTO, t1, c1);
-
-		t1.getList().add(os1);
-		c1.getList().add(os1);
-		
-		tecnicoRepository.saveAll(Arrays.asList(t1));
-		clienteRepository.saveAll(Arrays.asList(c1));
-		osRepository.saveAll(Arrays.asList(os1));
-	}
 }
---
+++ src/main/java/br/com/marcos/servcices/DBService.java
@@ -0,0 +1,41 @@
+package br.com.marcos.servcices;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.marcos.domain.Cliente;
+import br.com.marcos.domain.OS;
+import br.com.marcos.domain.Tecnico;
+import br.com.marcos.domain.enuns.Prioridade;
+import br.com.marcos.domain.enuns.Status;
+import br.com.marcos.repository.ClienteRepository;
+import br.com.marcos.repository.OSRepository;
+import br.com.marcos.repository.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private OSRepository osRepository;
+
+	public void instaciaDB() {
+		Tecnico t1 = new Tecnico(null, "Marcos Gabriel", "826.074.260-81", "(74) 96666-8958");
+		Cliente c1 = new Cliente(null, "João", "604.943.160-47", "(78) 96666-5252");
+		OS os1 = new OS(null, Prioridade.ALTA, "Teste create os", Status.ADAMENTO, t1, c1);
+
+		t1.getList().add(os1);
+		c1.getList().add(os1);
+
+		tecnicoRepository.saveAll(Arrays.asList(t1));
+		clienteRepository.saveAll(Arrays.asList(c1));
+		osRepository.saveAll(Arrays.asList(os1));
+	}
+}
