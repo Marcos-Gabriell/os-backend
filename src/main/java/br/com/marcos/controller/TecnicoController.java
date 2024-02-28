5aa7708bbef781046fd8bcfc3b5da2eac1c99425
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-28 18:40:27 -0300

    Usando o validation

--- src/main/java/br/com/marcos/controller/TecnicoController.java
+++ src/main/java/br/com/marcos/controller/TecnicoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.Servlet;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -45,7 +46,7 @@ public class TecnicoController {
 	}
 
 	@PostMapping()
-	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO) {
+	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
 		Tecnico newObj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 
