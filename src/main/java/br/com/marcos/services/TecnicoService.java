1c71c6ef3e6d3be6c005830147731f861300baa0
mgabr <mgabr@Marcos>
2024-02-29 23:06:10 -0300

    começando a criar método put do tecnico

--- src/main/java/br/com/marcos/services/TecnicoService.java
+++ src/main/java/br/com/marcos/services/TecnicoService.java
@@ -3,6 +3,8 @@ package br.com.marcos.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,6 +35,12 @@ public class TecnicoService {
 			throw new DataIntegratyViolationExeception("CPF já cadastrado na base de dados!");
 		}
 		return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
+	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		Tecnico oldObj = findById(id);
+		
+		
 	}
 	
 	private Tecnico findByCPF(TecnicoDTO objDTO) {
