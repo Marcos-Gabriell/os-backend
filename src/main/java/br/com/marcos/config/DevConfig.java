248ea901c838ec157274146897e022bbb0d92051
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-03-06 21:51:44 -0300

    Correção de problema de CORS

--- src/main/java/br/com/marcos/config/DevConfig.java
+++ src/main/java/br/com/marcos/config/DevConfig.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.marcos.services.DBService;
 
 @Configuration
 @Profile("dev")
-public class DevConfig {
+public class DevConfig implements WebMvcConfigurer {
 
 
 	@Autowired
@@ -27,4 +29,11 @@ public class DevConfig {
 		
 		return false;
 	}
+	
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedOrigins("http://localhost:4200")
+	                .allowedMethods("GET", "POST", "PUT", "DELETE");
+	    }
 }
